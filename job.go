2da325dc848a08d240c08cf9b77d23503e049375
Ashwin Srinath <atrikut@clemson.edu>
2016-12-02 14:58:37 -0500

    run gofmt

--- job.go
+++ job.go
@@ -3,13 +3,13 @@ package appm
 import "fmt"
 
 type Job struct {
-    Id string
-    User string
-    Ncpus int
-    Mem int
-    Walltime int
+	Id       string
+	User     string
+	Ncpus    int
+	Mem      int
+	Walltime int
 }
 
 func (j Job) String() string {
-    return fmt.Sprintf("%v\t%v\t%v\t%vgb\t%v", j.Id, j.User, j.Ncpus, j.Mem, SecondsToHMS(j.Walltime))
+	return fmt.Sprintf("%v\t%v\t%v\t%vgb\t%v", j.Id, j.User, j.Ncpus, j.Mem, SecondsToHMS(j.Walltime))
 }
--- timeutil.go
+++ timeutil.go
@@ -1,46 +1,46 @@
 package appm
 
 import (
-    "strings"
-    "strconv"
-    "log"
-    "fmt"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
-func HMSToSeconds (s string) (int, error) {
-    // HMSToSeconds parses a duration string of the form H..HH:MM:SS.
-    // and returns its value in seconds.
-    if strings.Count(s, ":") != 2 {
-        err := fmt.Errorf("Expected duration string of the form H..HH:MM:SS; received: %q", s)
-        return 0, err
-    }
+func HMSToSeconds(s string) (int, error) {
+	// HMSToSeconds parses a duration string of the form H..HH:MM:SS.
+	// and returns its value in seconds.
+	if strings.Count(s, ":") != 2 {
+		err := fmt.Errorf("Expected duration string of the form H..HH:MM:SS; received: %q", s)
+		return 0, err
+	}
 
-    hms := strings.Split(s, ":")
-    if len(hms) != 3 {
-        err := fmt.Errorf("Expected duration string of the form H..HH:MM:SS; received: %q", s)
-        return 0, err
-    }
+	hms := strings.Split(s, ":")
+	if len(hms) != 3 {
+		err := fmt.Errorf("Expected duration string of the form H..HH:MM:SS; received: %q", s)
+		return 0, err
+	}
 
-    hours, err := strconv.Atoi(hms[0])
-    if err != nil {
-        log.Fatal(err)
-    }
-    minutes, err := strconv.Atoi(hms[1])
-    if err != nil {
-        log.Fatal(err)
-    }
-    seconds, err := strconv.Atoi(hms[2])
-    if err != nil {
-        log.Fatal(err)
-    }
+	hours, err := strconv.Atoi(hms[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	minutes, err := strconv.Atoi(hms[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	seconds, err := strconv.Atoi(hms[2])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return hours*3600 + minutes*60 + seconds, nil
+	return hours*3600 + minutes*60 + seconds, nil
 }
 
 func SecondsToHMS(sec int) string {
-    // 
-    hours := sec/3600
-    minutes := (sec - 3600*hours)/60
-    seconds := sec - 3600*hours - 60*minutes
-    return fmt.Sprintf("%02v:%02v:%02v", hours, minutes, seconds)
+	//
+	hours := sec / 3600
+	minutes := (sec - 3600*hours) / 60
+	seconds := sec - 3600*hours - 60*minutes
+	return fmt.Sprintf("%02v:%02v:%02v", hours, minutes, seconds)
 }
